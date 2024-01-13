516c99ecdc54e1deb6fb4114a66b3e742b1a9d7c
ratul-space <to.ratul.hosen@gmail.com>
2024-01-13 12:31:07 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlin Control Flow Part 2"
+include(":app")
+ 
