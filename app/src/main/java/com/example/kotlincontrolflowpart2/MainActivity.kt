e1fbff309faf28dfe4a267adc1581dc807c38d42
ratul-space <to.ratul.hosen@gmail.com>
2024-01-13 12:57:42 +0600

    feat: uses of for loop

--- app/src/main/java/com/example/kotlincontrolflowpart2/MainActivity.kt
+++ app/src/main/java/com/example/kotlincontrolflowpart2/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.kotlincontrolflowpart2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var result = ""
+        for (index in 0..18) {
+            result += index.toString() + "\n"
+        }
+        var tvResult = findViewById<TextView>(R.id.tvResult)
+        tvResult.text = result
+
     }
 }
